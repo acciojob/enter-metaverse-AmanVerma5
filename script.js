1d5b4c67e038fc6f34ab501606aca67bebe80979
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-24 09:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 //your JS code here. If required.
  function enterMetaverse(){
-    let stat = document.getElementById("status");
-	 var h1Tag = document.createElement("h1");
-	 h1Tag.id="status"
-     h1Tag.textContent = "Entered Metaverse";
-     stat.replaceWith(h1Tag);
+     document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
  }
