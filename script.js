9e9510b28911e8e857fd1e3a7f3d74f7d2bed8a7
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-24 09:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ function enterMetaverse(){
+    let stat = document.getElementById("status");
+	 var h1Tag = document.createElement("h1");
+     h1Tag.textContent = "Entered Metaverse";
+     stat.replaceWith(h1Tag);
+ }
